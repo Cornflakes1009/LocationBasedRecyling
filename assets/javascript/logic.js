cac7a49ac661773b07aa10cc7c5e6a8cf7a1d0f9
Harold Davidson <harold.davidson89@gmail.com>
2018-03-07 00:17:47 -0600

    form added and some checkboxes put in

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,5 +1,6 @@
 
 $( document ).ready(function() {
+    
 
     var config = {
         apiKey: "AIzaSyB7Tjvl7pf3Acdlon_3pUL0OdECPOAIjsw",
@@ -12,23 +13,29 @@ $( document ).ready(function() {
       firebase.initializeApp(config);
     
         
-      function initMap() {
-        // Create a map object and specify the DOM element for display.
-        var map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: -34.397, lng: 150.644},
-          zoom: 8
-        });
-      }
+    //   function initMap() {
+    //     // Create a map object and specify the DOM element for display.
+    //     var map = new google.maps.Map(document.getElementById('map'), {
+    //       center: {lat: -34.397, lng: 150.644},
+    //       zoom: 8
+    //     });
+    //   }
     
     
     
+      $("#map").on('click', function(){
+        // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
+        $('#modal1').modal();
+      });
     
-    
+      
     
     
     
     
     
     }); // end of document ready function
+
+    
     
     
