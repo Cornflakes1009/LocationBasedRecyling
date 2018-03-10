6a8028950c7bd0dd54405fb0354d517122c3d3b0
Ted <ted_hodges@ymail.com>
2018-03-10 13:02:40 -0600

    minor tweaks to modal in preparation for usage

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -185,7 +185,9 @@ $( document ).ready(function() {
 
     //If user has incorrectly entered address, here's the error modal
 
-    $('#modal2').modal({
+    // Place this in Google Maps info:   $('#modal2').modal('open');
+
+    $('.modal').modal({
       dismissible: true, // Modal can be dismissed by clicking outside of the modal
       opacity: .5, // Opacity of modal background
       inDuration: 300, // Transition in duration
@@ -193,10 +195,9 @@ $( document ).ready(function() {
       startingTop: '4%', // Starting top style attribute
       endingTop: '10%', // Ending top style attribute
       ready: function(modal, trigger) { // Callback for Modal open. Modal and trigger parameters available.
-        alert("Ready");
-        console.log(modal, trigger);
+       
       },
-      complete: function() { alert('Closed'); } // Callback for Modal close
+      complete: function() {  } // Callback for Modal close
     }
   );
 
