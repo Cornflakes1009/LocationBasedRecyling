fe150852c956d2cdf5746f7d21c19f2af6d4ea6c
UMKC CTCFP User <umkc-ctcfpuser@umkc.edu>
2018-03-10 23:58:10 -0600

    Added logic to link google api with firebase and user entered info

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,91 +1,63 @@
 
 var map;
 var infoWindow;
-var service;
-function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: { lat: 39.102115, lng: -94.582554 },
-    zoom: 10
-  });
-  infoWindow = new google.maps.InfoWindow;
+var geocoder;
+var isAdd;
+      function initMap() {
+        //added geocoder to this function
+        geocoder = new google.maps.Geocoder();
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: 39.102115, lng: -94.582554},
+          zoom: 10
+        });
+        infoWindow = new google.maps.InfoWindow;
 
-  // Try HTML5 geolocation.
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function (position) {
-      var pos = {
-        lat: position.coords.latitude,
-        lng: position.coords.longitude
-      };
+        // Try HTML5 geolocation.
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            };
 
-      infoWindow.setPosition(pos);
-      infoWindow.setContent('Location found.');
-      infoWindow.open(map);
-      map.setCenter(pos);
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('Location found.');
+            infoWindow.open(map);
+            map.setCenter(pos);
 
-      //request variable along with service.textSearch finds recycling centers within 
-      //500 meters of current location
-
-      var request = {
-        location: pos,
-        radius: '500',
-        query: 'recycling centers'
-      };
-
-      service = new google.maps.places.PlacesService(map);
-      service.textSearch(request, callback);
-
-      //end code for adding recyling centers
-
-    }, function () {
-      handleLocationError(true, infoWindow, map.getCenter());
-    });
-  } else {
-    // Browser doesn't support Geolocation
-    handleLocationError(false, infoWindow, map.getCenter());
-  }
-
-}
-
-function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-  infoWindow.setPosition(pos);
-  infoWindow.setContent(browserHasGeolocation ?
-    'Error: The Geolocation service failed.' :
-    'Error: Your browser doesn\'t support geolocation.');
-  infoWindow.open(map);
-}
-
-//additional code for adding markers for recylcing centers - gets the placeid for the getDetails
-function callback(results, status) {
-  if (status == google.maps.places.PlacesServiceStatus.OK) {
-    for (var i = 0; i < results.length; i++) {
-      var place = results[i];
-      var idPlace = results[i].place_id;
-
-      // gets the name and address and creates the pin with this info
-      service.getDetails({
-        placeId: idPlace
-      }, function (place, status) {
-        if (status === google.maps.places.PlacesServiceStatus.OK) {
-          var marker = new google.maps.Marker({
-            map: map,
-            position: place.geometry.location
-          });
-          google.maps.event.addListener(marker, 'click', function () {
-            infoWindow.setContent('<div><strong>' + place.name + '</strong><br>' +
-              place.formatted_address + '</div>');
-            infoWindow.open(map, this);
-          });
-        }
-      });
+          })
+        } 
       }
-      // checking if the accepted items array is empty and if not, pushing that to firebase
-      // if (acceptedItems !== []){
-      //   database.ref().push({
-      //     acceptedItems: acceptedItems
-      // });
-      // }
-      console.log(acceptedItems);
-    })
+
+      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+        infoWindow.setPosition(pos);
+        infoWindow.setContent(browserHasGeolocation ?
+                              'Error: The Geolocation service failed.' :
+                              'Error: Your browser doesn\'t support geolocation.');
+        infoWindow.open(map);
+      }
+    
+      function codeAddress(address, containerSize, acceptedItems, isAdd, callback) {
+        //var address = document.getElementById('address').value;
+        var isValid;
+        geocoder.geocode( { 'address': address}, function(results, status) {
+          if (status == 'OK') {
+                       
+            var marker = new google.maps.Marker({
+                map: map,
+                position: results[0].geometry.location
+            })
+            isValid = true;
+          } else {
+            isValid = false;
+            
+          }
+          if (isAdd) {
+          callback(address, containerSize, acceptedItems, isValid)
+          }
+        });
+      }
+
 
     //If user has incorrectly entered address, here's the error modal
 
@@ -104,9 +76,7 @@ function callback(results, status) {
       complete: function() {  } // Callback for Modal close
 
 
-    }
-  }
-}
+    })
 
 //end of code for adding markers for recylcing centers
 
@@ -131,11 +101,10 @@ $(document).ready(function () {
     console.log('add button clicked');
   });
 
-  $('#submit-button').on('click', function () {
+  $('#submit-button').on('click', function (event) {
     console.log('submit button clicked');
-    var address = $("#address").val().trim();
-
-    var address = $('#address').val().trim();
+    event.preventDefault();
+    var address = $("#address").val().trim();;
 
     // container sizes
     var small;
@@ -191,14 +160,24 @@ $(document).ready(function () {
       acceptedItems.push(steel);
     }
     // checking if the accepted items and container size arrays arrays are empty and if not, pushing them to firebase
-    if (containerSize.length > 0 && acceptedItems.length > 0 && address !== "") {
-
-      console.log(containerSize, acceptedItems);
-      database.ref().push({
-        containerSize: containerSize,
-        acceptedItems: acceptedItems,
-        address: address
-      });
+    isAdd = true;
+    codeAddress(address, containerSize, acceptedItems, isAdd, callback);
+      function callback(address, containerSize, acceptedItems, isValid) {
+      if (containerSize.length > 0 && acceptedItems.length > 0 && address !== "") {
+        if (isValid) {
+          console.log(containerSize, acceptedItems);
+          database.ref().push({
+            containerSize: containerSize,
+            acceptedItems: acceptedItems,
+            address: address
+        
+          })
+        } else {
+          //need to open error modal here
+          console.log("invalid address");
+          $('#modal2').modal();
+        }
+      }
     }
     // checking if the accepted items array is empty and if not, pushing that to firebase
     // if (acceptedItems !== []){
@@ -226,6 +205,8 @@ $(document).ready(function () {
   }
   );
   database.ref().on('child_added', function (childSnapshot) {
+    isAdd = false;
+    codeAddress(childSnapshot.val().address, isAdd);
     console.log('running');
     var newDiv = $("<div>");
     newDiv.addClass("col s12");
@@ -233,11 +214,5 @@ $(document).ready(function () {
     $('#location-section').append(newDiv);
 
   });
+
 }); // end of document ready function
-// + childSnapshot.val().containerSize + childSnapshot.val().acceptedItems + childSnapshot.val().address +
-
-// var newDiv = $("<div>");
-// newDiv.addClass("col s12");
-// newDiv.text("some text");
-
-// $("#location-section").append(newDiv);
